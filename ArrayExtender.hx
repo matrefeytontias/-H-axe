f5ce9c0c30f40c228cb5c65ee46b3b066f69ab69
matrefeytontias <mattias@refeyton.fr>
2018-04-25 19:16:36 +0200

    It's gonna be an AST dammit

---
+++ ArrayExtender.hx
@@ -0,0 +1,28 @@
+class ArrayExtender
+{
+    static public function startsWith<T>(array:Array<T>, test:Array<T>) : Bool
+    {
+        if(test.length <= array.length)
+        {
+            for(i in 0 ... test.length)
+                if(test[i] != array[i])
+                    return false;
+            return true;
+        }
+        else
+            return false;
+    }
+    
+    static public function startConstructedWith<T:EnumValue>(array:Array<T>, test:Array<String>) : Bool
+    {
+        if(test.length <= array.length)
+        {
+            for(i in 0 ... test.length)
+                if(Type.enumConstructor(array[i]) != test[i])
+                    return false;
+            return true;
+        }
+        else
+            return false;
+    }
+}
--- Lexer.hx
+++ Lexer.hx
@@ -1,45 +1,17 @@
+import haxe.Constraints;
 import haxe.ds.GenericStack;
 import haxe.io.Bytes;
 
 import Rules;
-import context.*;
+
+using ArrayExtender;
 
 /**
- * Axe being a naturally sequential language, it is easier to interpret it as an
- * array of tokens rather than an AST. The present implementation uses a finite-
- * state machine to execute its task.
+ * Builds an AST for the Axe language.
  */
 class Lexer
 {
-    /**
-     * Representation of memory as an array of unsigned bytes.
-     */
-    private var memory:Bytes;
-    /**
-     * Current context. Holds elements, eg variables and functions.
-     */
-    private var context:Map<String, Element> = new Map();
-    
-    /**
-     * Accumulation register 1.
-     */
-    private var hl:Int;
-    /**
-     * Accumulation register 2.
-     */
-    private var de:Int;
-    
-    /**
-     * Program counter, ie offset in the stream of tokens to the next instruction.
-     */
-    private var pc:Int;
-    
-    /**
-     * PC stack. Meant to hold the return addresses of function calls.
-     */
-    private var pcStack:GenericStack<Int> = new GenericStack();
-    
-    static public function execute(input:Array<Tokens>)
+    static public function buildAST(input:Array<Tokens>)
     {
         
     }
